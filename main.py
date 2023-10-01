49c5ccde87a7d1edefa5d85a153e2afc24d3e766
davidenders11 <david.enders@xostrucks.com>
2023-09-30 23:53:08 -0700

    modify main to use args

--- main.py
+++ main.py
@@ -16,23 +16,22 @@ parser = argparse.ArgumentParser(
 # stores a bool with the value of True if the flag is present, and False if it is not
 parser.add_argument("--reply", action="store_true", help="Reply to the most recent email thread from the specified recipient.")
 parser.add_argument("recipient", type=str, help="The email address of the recipient.")
-parser.parse_args()
+args = parser.parse_args()
 
 def main():
     creds = gmail_functions.auth()
     openai_functions.auth()
     service = build("gmail", "v1", credentials=creds)
     me = get_own_email(service)
-    other = sys.argv[1]
-    query = f"from:{other}"
+    query = f"from:{args.recipient}"
     last_thread_id = get_most_recent_message(service, query)
     thread = get_thread(service, last_thread_id)
-    update = input(f"What would you like to tell {other}?\n")
+    update = input(f"What would you like to tell {args.recipient}?\n")
     subject = input(f"\nWhat would you like the subject of the email to be?\n")
-    content = write_draft(thread, update, me, other)
-    gmail_create_draft(service, subject, content, me, other)
+    content = write_draft(thread, update, me, args.recipient)
+    gmail_create_draft(service, subject, content, me, args.recipient)
     print(
-        f"\nYour draft has been created!\nRecipient: {other}\nSubject: {subject}\nContent: {content}\n"
+        f"\nYour draft has been created!\nRecipient: {args.recipient}\nSubject: {subject}\nContent: {content}\n"
     )
 
 
--- parsetest.py
+++ parsetest.py
@@ -5,6 +5,6 @@ parser = argparse.ArgumentParser(
 parser.add_argument("--reply", action="store_true", help="Reply to the most recent email thread from the specified recipient.")
 parser.add_argument("recipient", type=str, help="The email address of the recipient.")
 
-args = parser.parse_args()
 
-print(args)
+args = parser.parse_args()
+print(args.reply, args.recipient)
