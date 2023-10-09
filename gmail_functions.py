e0c3e9f2209569d83163ea5c42a30af9ba1ec2ce
davidenders11 <david.enders@xostrucks.com>
2023-10-09 09:10:48 -0700

    oopify gmail

--- gmail_functions.py
+++ gmail_functions.py
@@ -3,7 +3,6 @@ from __future__ import print_function
 import os.path
 import json
 import sys
-
 import base64
 from base64 import urlsafe_b64decode, urlsafe_b64encode
 import google.auth
@@ -15,6 +14,7 @@ from googleapiclient.errors import HttpError
 
 from email.message import EmailMessage
 
+
 # If modifying these scopes, delete the file token.json.
 SCOPES = [
     "https://www.googleapis.com/auth/gmail.readonly",
@@ -22,99 +22,123 @@ SCOPES = [
 ]
 
 
+class Gmail:
 
-def auth(logger):
-    """
-    Authenticates user and saves token.json
-    """
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first time.
-    if os.path.exists("token.json"):
-        logger.info("Reading credentials from existing token.json")
-        creds = Credentials.from_authorized_user_file("token.json", SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            logger.info("Credentials expired, sending refresh request")
-            creds.refresh(Request())
-        else:
-            logger.info("Credentials not found, sending auth request to local server")
-            flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open("token.json", "w") as token:
-            logger.info("Saving credentials to token.json")
-            token.write(creds.to_json())
-    return creds
+    def __init__(self, logger):
+        self.logger = logger
+        self.auth()
+        self.me = self.service.users().getProfile(userId="me").execute()["emailAddress"]
 
+    def auth(self):
+        creds = None
+        # The file token.json stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first time.
+        if os.path.exists("token.json"):
+            self.logger.info("Reading credentials from existing token.json")
+            creds = Credentials.from_authorized_user_file("token.json", SCOPES)
+        # If there are no (valid) credentials available, let the user log in.
+        if not creds or not creds.valid:
+            if creds and creds.expired and creds.refresh_token:
+                self.logger.info("Credentials expired, sending refresh request")
+                creds.refresh(Request())
+            else:
+                self.logger.info("Credentials not found, sending auth request to local server")
+                flow = InstalledAppFlow.from_client_secrets_file("credentials.json", SCOPES)
+                creds = flow.run_local_server(port=0)
+            # Save the credentials for the next run
+            with open("token.json", "w") as token:
+                self.logger.info("Saving credentials to token.json")
+                token.write(creds.to_json())
+        self.service = build("gmail", "v1", credentials=creds)
+        self.logger.info("Completed Gmail authentication flow")
 
-def get_most_recent_message(service, query, logger):
-    logger.info(f"Querying Gmail for most recent message with query: {query}")
-    result = service.users().messages().list(userId="me", q=query).execute()
-    messages = []
-    if "messages" in result:
-        messages.extend(result["messages"])
-    while "nextPageToken" in result:
-        page_token = result["nextPageToken"]
-        result = (
-            service.users()
-            .messages()
-            .list(userId="me", q=query, pageToken=page_token)
-            .execute()
-        )
+    def get_most_recent_message(self, query):
+        self.logger.info(f"Querying Gmail for most recent message with query: {query}")
+        result = self.service.users().messages().list(userId="me", q=query).execute()
+        messages = []
         if "messages" in result:
             messages.extend(result["messages"])
-    return messages[0]["threadId"]
+        while "nextPageToken" in result:
+            page_token = result["nextPageToken"]
+            result = (
+                self.service.users()
+                .messages()
+                .list(userId="me", q=query, pageToken=page_token)
+                .execute()
+            )
+            if "messages" in result:
+                messages.extend(result["messages"])
+        return messages[0]["threadId"]
 
+    def get_thread(self, thread_id):
+        """
+        Get a thread and print each message including its sender and body
+        """
+        response = self.service.users().threads().get(userId="me", id=thread_id).execute()
+        messages = response["messages"]
+        thread = ""
+        for message in messages:
 
-def get_thread(service, thread_id):
-    """
-    Get a thread and print each message including its sender and body
-    """
-    response = service.users().threads().get(userId="me", id=thread_id).execute()
-    messages = response["messages"]
-    thread = ""
-    for message in messages:
+            # specify sender in final string
+            for header in message["payload"]["headers"]:
+                if header["name"] == "From":
+                    thread += f"From: {header['value']}"
+            
+            # get messages content and add to string
+            for part in message["payload"]["parts"]:
+                if part["mimeType"] == "text/plain":
+                    content = urlsafe_b64decode(part["body"]["data"]).decode()
+                    # remove any lines that start with ">" as these are redundant
+                    content = "\n".join(
+                        [line for line in content.split("\n") if not line.startswith(">")]
+                    )
+                    thread += f"Body:\n{content}"
+                elif part["mimeType"] == "multipart/alternative":
+                    for subpart in part["parts"]:
+                        if subpart["mimeType"] == "text/plain":
+                            content = urlsafe_b64decode(subpart["body"]["data"]).decode()
+                            # remove any lines that start with ">"
+                            content = "\n".join(
+                                [
+                                    line
+                                    for line in content.split("\n")
+                                    if not line.startswith(">")
+                                ]
+                            )
+                            thread += f"Body:\n{content}"
+                            break
 
-        # specify sender in final string
-        for header in message["payload"]["headers"]:
-            if header["name"] == "From":
-                thread += f"From: {header['value']}"
-        
-        # get messages content and add to string
-        for part in message["payload"]["parts"]:
-            if part["mimeType"] == "text/plain":
-                content = urlsafe_b64decode(part["body"]["data"]).decode()
-                # remove any lines that start with ">" as these are redundant
-                content = "\n".join(
-                    [line for line in content.split("\n") if not line.startswith(">")]
-                )
-                thread += f"Body:\n{content}"
-            elif part["mimeType"] == "multipart/alternative":
-                for subpart in part["parts"]:
-                    if subpart["mimeType"] == "text/plain":
-                        content = urlsafe_b64decode(subpart["body"]["data"]).decode()
-                        # remove any lines that start with ">"
-                        content = "\n".join(
-                            [
-                                line
-                                for line in content.split("\n")
-                                if not line.startswith(">")
-                            ]
-                        )
-                        thread += f"Body:\n{content}"
-                        break
+        return thread
+    
 
-    return thread
+    def gmail_create_draft(self, subject, content, other):
+        """Create and insert a draft email.
+        Print the returned draft's message and id.
+        Returns: Draft object, including draft id and message meta data.
+        """
+        try:
+            message = EmailMessage()
 
+            message.set_content(content)
 
-def get_own_email(service):
-    """
-    Get own email address
-    """
-    response = service.users().getProfile(userId="me").execute()
-    return response["emailAddress"]
+            message["To"] = other
+            message["From"] = self.me
+            message["Subject"] = subject
+
+            # encoded message
+            encoded_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+
+            create_message = {"message": {"raw": encoded_message}}
+            draft = (
+                self.service.users().drafts().create(userId="me", body=create_message).execute()
+            )
+
+        except HttpError as error:
+            print(f"An error occurred: {error}")
+            draft = None
+
+        return draft
+
 
 
 def gmail_create_draft(service, subject, content, me, other):
--- main.py
+++ main.py
@@ -1,14 +1,7 @@
 import sys
 import argparse
-import gmail_functions
 import logging
-import openai_functions
-from gmail_functions import (
-    get_thread,
-    get_own_email,
-    get_most_recent_message,
-    gmail_create_draft,
-)
+from gmail_functions import Gmail
 from openai_functions import OpenAI
 from googleapiclient.discovery import build
 
@@ -25,29 +18,21 @@ logging.basicConfig(
         format = '%(levelname)s:%(asctime)s:%(message)s')
 logger = logging.getLogger(__name__)
 
-def main():
-    creds = gmail_functions.auth(logger)
-    logger.info("Completed Gmail Authentication flow")
-    
+def main():    
     openai = OpenAI(logger)
-
-    service = build("gmail", "v1", credentials=creds)
-    logger.info("Built Gmail endpoint service")
-
-    me = get_own_email(service)
-    logger.info("Retrieved user email")
+    gmail = Gmail(logger)
 
     query = f"from:{args.recipient}"
-    last_thread_id = get_most_recent_message(service, query, logger)
-    thread = get_thread(service, last_thread_id)
+    last_thread_id = gmail.get_most_recent_message(query)
+    thread = gmail.get_thread(last_thread_id)
     logger.info("Retrieved last thread with target recipient")
 
     update = input(f"What would you like to tell {args.recipient}?\n")
     subject = input(f"\nWhat would you like the subject of the email to be?\n")
-    content = openai.write_draft(thread, update, me, args.recipient)
+    content = openai.write_draft(thread, update, gmail.me, args.recipient)
     logger.info("Draft has been generated, OpenAI call complete")
 
-    gmail_create_draft(service, subject, content, me, args.recipient)
+    gmail.gmail_create_draft(subject, content, args.recipient)
     print(
         f"\nYour draft has been created!\nRecipient: {args.recipient}\nSubject: {subject}\nContent: {content}\n"
     )
